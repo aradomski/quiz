3f7d9d24b37c3ef983eb0a9370bb1a96a9724537
Adam <niewpisze@gmail.com>
2012-03-03 19:16:24 +0100

    'Skrypt commitujacy'

--- repo.sh
+++ repo.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
+if [ -z '$1' ] ; then
+	echo "Podaj opis commitu";
+else
+	nazwa=\'$1\';
+	echo $1;
+	echo $nazwa;
+	echo "*************Adding to git*************";
+	git add . && git commit -m "$nazwa" && git push 
+	echo "*************Adding to mercurial*************";
+	hg add . && hg commit -m "$nazwa" && hg push
+fi
 
-git add .
-hg add .
-git commit -m '$1'
-hg commit -m '$1'
-git push 
-hg push
