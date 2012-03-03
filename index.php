3366a89d46756abe18896ffbb303bf5ef9fccf18
Adam <niewpisze@gmail.com>
2012-03-03 19:59:38 +0100

    favicon

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 <html>
 	<head>
 		<meta charset=utf-8>
-		<link rel="Shortcut icon" href="images/domek.gif" />
-		<link rel="icon" type="image/png" href="images/domek.gif"/>
+		<link rel="Shortcut icon" href="style/images/fav.png" />
+		<link rel="icon" type="image/png" href="style/images/fav.png"/>
 		
-		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>		
+		<link rel="stylesheet" type="text/css" href="style/styl.css" />
+		
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>	
+		<script type="text/javascript" src="js/skrypt.js"></script>	
 	
-		<title></title>
+		<title>You shall not pass!</title>
 	</head>
 	<body>
+		<?php
+			echo $path = $_SERVER['DOCUMENT_ROOT']; 
+		?>
 	</body>
 </html>
 
 
-<?php
-echo $path = $_SERVER['DOCUMENT_ROOT']; 
-?>
