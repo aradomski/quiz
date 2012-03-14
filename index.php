f50ebcc7f7a42a19923eb9bbc734877805b9dc80
Adam <niewpisze@gmail.com>
2012-03-14 22:14:31 +0100

    bla

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<link rel="Shortcut icon" href="style/images/fav.png" />
 		<link rel="icon" type="image/png" href="style/images/fav.png"/>
 		
+		<link rel="stylesheet" type="text/css" href="style/normalize.css" />
 		<link rel="stylesheet" type="text/css" href="style/styl.css" />
 		
 		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>	
@@ -14,11 +15,24 @@
 	</head>
 	<body>
 		<header>Naglowek:)</header>
-		<section>
-			<article id="login">
+		<section id="content">
+			<article id="leftMenu">
 			<?php
 				echo $path = $_SERVER['DOCUMENT_ROOT']; 
 			?>
+			</article>
+			<article id="quiz">
+				<section id="question">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra, nunc ac lacinia varius, nisl elit vehicula nisl, a aliquet quam ante eleifend magna. Etiam pretium, enim non cursus auctor, lacus felis ornare nulla, sit amet mattis neque neque a urna. Ut et quam lacinia diam vestibulum laoreet. Nunc vel lacus sit amet mi molestie ultrices. Fusce sollicitudin tempus dui sit amet sodales. Mauris condimentum commodo ipsum, lacinia consectetur turpis rhoncus suscipit. Sed mattis lacinia magna, et pulvinar urna bibendum eget.
+					<ul id="anwser">
+						<li id="anwserA"><h1>A</h1><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
+						<li id="anwserB"><h1>B</h1><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a libero eget enim venenatis iaculis in vel lacus. Quisque sapien lacus, dapibus in ultrices eu, facilisis ac ipsum. Maecenas ut ante magna, eget auctor elit. Morbi tempor venenatis nunc, eget cursus eros gravida nec. Aenean vel mauris quam. Integer ultrices, augue et porta imperdiet, ante lectus interdum ipsum, in sagittis mauris ligula vitae tortor. Duis quis arcu in nisi consequat feugiat in non quam. 
+
+ Ut mollis pellentesque velit, eget semper neque tempor at. Suspendisse euismod vestibulum ullamcorper. Suspendisse vehicula volutpat sapien, eget hendrerit mauris cursus sed. Nunc a ante lectus, vel lacinia lacus. Maecenas pretium, arcu sed sollicitudin bibendum, enim augue tincidunt orci, nec vehicula nisl nisi vel libero. Curabitur ligula odio, semper nec viverra quis, lacinia et lectus. Morbi eu dui risus. Aliquam leo nunc, convallis et suscipit sed, sagittis ut libero. Maecenas eget tortor eu libero dignissim rhoncus. Nunc ultricies sem non sem varius et imperdiet lectus fringilla. Aenean sollicitudin viverra libero, quis blandit massa dapibus vel. Nam accumsan mi ac dolor ultricies id laoreet elit tincidunt. Fusce ac enim sed nulla molestie egestas ut ut nunc. In venenatis justo nec odio porta et semper ipsum vulputate.</span></li>
+						<li id="anwserC"><h1>C</h1><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
+						<li id="anwserD"><h1>D</h1><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
+					</ul>
+				</section>
 			</article>
 		</section>
 		<footer>Stopka</footer>
