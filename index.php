78c3f242ce1fa806eef81733ede6b3d9671c601d
Adam <niewpisze@gmail.com>
2012-03-26 20:10:56 +0200

    formularze i style

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset=utf-8>
+		<meta charset="utf-8">
 		<link rel="Shortcut icon" href="style/images/fav.png" />
 		<link rel="icon" type="image/png" href="style/images/fav.png"/>
-		
 		
 		<link rel="stylesheet" type="text/css" href="style/styl.css" />
 		
@@ -17,22 +16,25 @@
 		<header>Naglowek:)</header>
 		<section id="content">
 			<article id="leftMenu">
-			<?php
-				echo $path = $_SERVER['DOCUMENT_ROOT']; 
-			?>
+				<form id="login" method="post">
+				Login:<input type="text" name="userName" />
+				Hasło:<input type="password" name="pass" />
+				<input type="submit" name="login" value="submit"/>
+				</form>
 			</article>
-			<form id="quiz">
+			<form id="quiz" method="get">
 				<section id="question">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra, nunc ac lacinia varius, nisl elit vehicula nisl, a aliquet quam ante eleifend magna. Etiam pretium, enim non cursus auctor, lacus felis ornare nulla, sit amet mattis neque neque a urna. Ut et quam lacinia diam vestibulum laoreet. Nunc vel lacus sit amet mi molestie ultrices. Fusce sollicitudin tempus dui sit amet sodales. Mauris condimentum commodo ipsum, lacinia consectetur turpis rhoncus suscipit. Sed mattis lacinia magna, et pulvinar urna bibendum eget.
 					<ul id="answerBlock">
-						<li id="answerA" class="answer"><input type="radio" name="anwser" value="A" /><h1 class="answerH1">A</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
-						<li id="answerB" class="answer"><input type="radio" name="anwser" value="B" /><h1 class="answerH1">B</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a libero eget enim venenatis iaculis in vel lacus. Quisque sapien lacus, dapibus in ultrices eu, facilisis ac ipsum. Maecenas ut ante magna, eget auctor elit. Morbi tempor venenatis nunc, eget cursus eros gravida nec. Aenean vel mauris quam. Integer ultrices, augue et porta imperdiet, ante lectus interdum ipsum, in sagittis mauris ligula vitae tortor. Duis quis arcu in nisi consequat feugiat in non quam. 
+						<li id="answerA" class="answer"><input type="radio" name="anwser" value="A" class="radioAnwser" /><h1 class="answerH1">A</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
+						<li id="answerB" class="answer"><input type="radio" name="anwser" value="B" class="radioAnwser" /><h1 class="answerH1">B</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a libero eget enim venenatis iaculis in vel lacus. Quisque sapien lacus, dapibus in ultrices eu, facilisis ac ipsum. Maecenas ut ante magna, eget auctor elit. Morbi tempor venenatis nunc, eget cursus eros gravida nec. Aenean vel mauris quam. Integer ultrices, augue et porta imperdiet, ante lectus interdum ipsum, in sagittis mauris ligula vitae tortor. Duis quis arcu in nisi consequat feugiat in non quam. 
 
  Ut mollis pellentesque velit, eget semper neque tempor at. Suspendisse euismod vestibulum ullamcorper. Suspendisse vehicula volutpat sapien, eget hendrerit mauris cursus sed. Nunc a ante lectus, vel lacinia lacus. Maecenas pretium, arcu sed sollicitudin bibendum, enim augue tincidunt orci, nec vehicula nisl nisi vel libero. Curabitur ligula odio, semper nec viverra quis, lacinia et lectus. Morbi eu dui risus. Aliquam leo nunc, convallis et suscipit sed, sagittis ut libero. Maecenas eget tortor eu libero dignissim rhoncus. Nunc ultricies sem non sem varius et imperdiet lectus fringilla. Aenean sollicitudin viverra libero, quis blandit massa dapibus vel. Nam accumsan mi ac dolor ultricies id laoreet elit tincidunt. Fusce ac enim sed nulla molestie egestas ut ut nunc. In venenatis justo nec odio porta et semper ipsum vulputate.</span></li>
-						<li id="answerC" class="answer"><input type="radio" name="anwser" value="C" /><h1 class="answerH1">C</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
-						<li id="answerD" class="answer"><input type="radio" name="anwser" value="D" /><h1 class="answerH1">D</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
+						<li id="answerC" class="answer"><input type="radio" name="anwser" value="C" class="radioAnwser" /><h1 class="answerH1">C</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
+						<li id="answerD" class="answer"><input type="radio" name="anwser" value="D" class="radioAnwser" /><h1 class="answerH1">D</h1><span class="answerSpan">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit.</span></li>
 					</ul>
 				</section>
+				<input type="submit" name="question" value="submit" />
 			</form>
 		</section>
 		<footer>
@@ -44,6 +46,9 @@
 				<p id="4"></p>
 				<p id="5"></p>
 			</section>
+			<?php
+				echo $path = $_SERVER['DOCUMENT_ROOT']; 
+			?>
 		</footer>
 	</body>
 </html>
--- js/skrypt.js
+++ js/skrypt.js
@@ -1,22 +1,21 @@
 /*globals $: false,
  document: false */
 $(document).on("click", "#answerBlock li", function () {
-	'use strict';
-	if ($(this).attr('class') === 'answer') {
-		$("#answerBlock .answerChecked").attr('class', 'answer');
-		$(this).attr('class', 'answerChecked');
-		$(this).children(":first").attr('checked', true);
-	} else {
-		$(this).attr('class', 'answer');
-		$(this).children(":first").attr('checked', false);
-	}
+    'use strict';
+    if ($(this).attr('class') === 'answer') {
+        $("#answerBlock .answerChecked").attr('class', 'answer');
+        $(this).attr('class', 'answerChecked');
+        $(this).children(":first").attr('checked', true);
+    } else {
+        $(this).attr('class', 'answer');
+        $(this).children(":first").attr('checked', false);
+    }
 });
 $(document).on("mouseover", "#answerBlock li", function () {
-	'use strict';
-	$('#1').text($(this).attr('class'));
+    'use strict';
+    $('#1').text($(this).attr('class'));
 });
 $(document).on("mouseout", "#answerBlock li", function () {
-	'use strict';
-	$('#1').text($(this).attr('class'));
+    'use strict';
+    $('#1').text($(this).attr('class'));
 });
-
